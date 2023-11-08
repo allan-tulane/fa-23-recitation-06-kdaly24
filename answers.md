3afca9c526bc3a56de73d78fe7a310d2bd42a792
Killian_Daly <kdaly3@tulane.edu>
2023-11-07 22:58:20 -0600

    Update 11/7 Final

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
+**Name:**_______Killian Daly___________
 **Name:**_________________________
 
 
@@ -10,11 +10,21 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+$W(n) = W(n-1) + W(n-2) + 1$
+
+This recursion can be solved, and is leaf dominated. This can be simplified to $O(2^n)$
 
 - **3)**
+$W(n) = W(n-1) + 1$
+
+This can be solved as $O(n)$
 
 - **4)**
+Due to the fact that n-1 iterates as well as n-2, the counts list shows us that many values in the fibonacci sequence are calculated twice, once from the W(n-1) branch and once from W(n-2)
 
 - **6)**
+In the new algorithm, each fib_town_down(i) in the fibonacci sequence is called once, as the list of fibs[n] keeps the previous used top down values. 
+With that, our Work is different, and can be reduced to W(n) as O(n). Span is the same, S(n) as O(n)
 
 - **8)**
+N times, as bottom up extends from 0 to n. With this in mind, work and span of Fib Bottom Up are $W(n)$ as $O(n)$ and $S(n)$ as $O(n)$
--- main.py
+++ main.py
@@ -4,20 +4,51 @@ def fib_recursive(n, counts):
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
     """    
-    counts[n] += 1
+    
+    if n == 0:
+        counts[n] += 1
+        return 0
+    elif n == 1:
+        counts[n] += 1
+        return 1
+    else:
+        counts[n] += 1
+        return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
+    
     ###TODO
-    pass
+    
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n in fibs:
+        return fibs[n]
+    else:
+        fibs_ntrack = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+        fibs[n] = fibs_ntrack
+        return fibs_ntrack
+    
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib_n = 1
+        fib_prev = 0
+
+        for i in range(2, n + 1):
+            temp = fib_n
+            fib_n = fib_n + fib_prev
+            fib_prev = temp
+
+        return fib_n
 
 
 
